edcadf9d0843889d9d868917e9082d5618d76f17
jb12-art <jbqw1@msn.com>
2026-03-23 10:38:55 +0200

    register.js file. - import {supabase} from supabase.js. - register form function. - - Other code that is not in use yet. - import {displayMessage} from ui.js. - success messages and error messages.

---
+++ js/register.js
@@ -0,0 +1,43 @@
+// js/register.js
+
+import { supabase } from './supabase.js';
+import { displayMessage } from './ui.js';
+
+const registerForm = document.querySelector('form');
+
+registerForm.addEventListener('submit', async function (e) {
+  e.preventDefault();
+
+  const form = e.target;
+  const email = form.email.value.trim();
+  const password = form.password.value;
+  const fieldset = form.querySelector('fieldset');
+
+  try {
+    fieldset.disabled = true;
+
+    const { data, error } = await supabase.auth.signUp({
+      email,
+      password,
+    });
+
+    if (error) {
+      displayMessage('#message-container', 'error', error.message);
+      return;
+    }
+
+    if (data.user) {
+      displayMessage(
+        '#message-container',
+        'success',
+        'Registration successful. Check your email to verify your account.',
+      );
+      form.reset();
+    }
+  } catch (error) {
+    console.log(error);
+    displayMessage('#message.container', 'error', error.toString());
+  } finally {
+    fieldset.disabled = false;
+  }
+});
