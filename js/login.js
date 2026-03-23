ff61f55c32e7485165fa90a330446518e783c312
jb12-art <jbqw1@msn.com>
2026-03-23 17:06:58 +0200

    login.js - login form. -display message.

--- js/login.js
+++ js/login.js
@@ -1,1 +1,39 @@
 // js/login.js
+
+import { supabase } from './supabase.js';
+import { displayMessage } from './ui.js';
+
+const loginForm = document.querySelector('form');
+
+loginForm.addEventListener('submit', async function (e) {
+  e.preventDefault();
+
+  const form = e.target;
+  const email = form.email.value.trim();
+  const password = form.password.value;
+  const fieldset = form.querySelector('fieldset');
+
+  try {
+    fieldset.disabled = true;
+
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) {
+      displayMessage('#js-message-container', 'error', error.message);
+      return;
+    }
+
+    if (data.user) {
+      console.log('user', data.user);
+      console.log('session', data.session);
+      location.href = 'index.html';
+    }
+  } catch (error) {
+    displayMessage('#js-message-container', 'error', error.toString());
+  } finally {
+    fieldset.disabled = false;
+  }
+});
