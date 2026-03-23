ea94f1dbb2f93f79ce0edc13a202e571c6f239a9
jb12-art <jbqw1@msn.com>
2026-03-23 10:29:58 +0200

    auth.js file. - import {supabase} from supabase.js. - export checkAuth(). - export logout().

---
+++ js/auth.js
@@ -0,0 +1,25 @@
+// js/auth.js
+
+import { supabase } from './supabase.js';
+
+export async function checkAuth() {
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    window.location.href = 'login.html';
+  }
+
+  console.log('user', user);
+}
+
+export async function logout() {
+  const { error } = await supabase.auth.signOut();
+
+  if (error) {
+    console.error('Error logging out:', error);
+  }
+
+  window.location.href = 'login.html';
+}
