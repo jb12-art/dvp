b261f566de2a94856203c61c387f14245fd6eeca
jb12-art <jbqw1@msn.com>
2026-03-23 10:44:10 +0200

    posts.js file. - import {supabase} from supabase.js. - import { displayMessage} from ui.js. - import {checkAuth, login} from auth.js. - checkAuth(). - register form function. - - Other code that is not in use yet. - import displayMessage from ui.js. - import logout from auth.js. - success and error messages.

---
+++ js/posts.js
@@ -0,0 +1,41 @@
+// js/posts.js
+
+import { supabase } from './supabase.js';
+import { displayMessage } from './ui.js';
+import { checkAuth, logout } from './auth.js';
+
+checkAuth();
+
+const postForm = document.querySelector('form');
+
+postForm.addEventListener('submit', async function (e) {
+  e.preventDefault();
+
+  const form = e.target;
+  const title = form.title.value.trim();
+  const content = form.content.value.trim();
+  const fieldset = form.querySelector('fieldset');
+
+  try {
+    fieldset.disabled = true;
+
+    const { error } = await supabase.from('posts').insert([{ title, content }]);
+
+    if (error) {
+      displayMessage('#message.container', 'error', error.message);
+      return;
+    }
+
+    displayMessage(
+      '#message-container',
+      'success',
+      'Post created successfully',
+    );
+    form.reset();
+  } catch (error) {
+    console.log(error);
+    displayMessage('#message-container', 'error', error.toString());
+  } finally {
+    fieldset.disabled = false;
+  }
+});
