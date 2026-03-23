d8358b5fa9b959fddb43adf04b711272c384a76d
jb12-art <jbqw1@msn.com>
2026-03-23 17:09:40 +0200

    ui.js - error message. - success message.

--- js/ui.js
+++ js/ui.js
@@ -1,1 +1,19 @@
 // js/ui.js
+export function displayMessage(container, messageType, message) {
+  let parent = container;
+
+  if (typeof container === 'string') {
+    parent = document.querySelector(container);
+  }
+
+  const messageClassess = {
+    error: 'message message--error',
+    success: 'message message--success',
+    warning: 'message message--warning',
+    info: 'message message--info',
+  };
+
+  const classes = messageClassess[messageType] || messageClassess.info;
+
+  parent.innerHTML = `<div class="${classes}">${message}</div>`;
+}
